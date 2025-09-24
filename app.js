3b4e0f6eb1c84d9b65d7674dc1e7b81c49f0e661
25maae <eaa25maae@students.eaaa.dk>
2025-09-24 12:54:29 +0200

    Opstart

---
+++ app.js
@@ -0,0 +1,5 @@
+document.body.innerHTML += "Hello JavaScript!";
+// alert("Hello Multimedia Designers");
+console.log("Hello Multimedia Designers");
+console.log("Hi Students!");
+console.log("This is your first JavaScript App 🎉");
